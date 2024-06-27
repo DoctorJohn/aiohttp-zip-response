ad4e88d3d85e4cfef6ecd708389989689b38afed
Jonathan Ehwald <github@ehwald.info>
2024-06-27 18:36:22 +0200

    Make it easier to filter member paths

--- aiohttp_zip_response/zip_response.py
+++ aiohttp_zip_response/zip_response.py
@@ -10,13 +10,13 @@ from stream_zip import ZIP_32, async_stream_zip, AsyncMemberFile
 class ZipResponse(web.StreamResponse):
     def __init__(
         self,
-        path: PathLike,
+        base_path: PathLike,
         chunk_size: int = 256 * 1024,
         status: int = 200,
         reason: Optional[str] = None,
         headers: Optional[LooseHeaders] = None,
     ) -> None:
-        self._path = Path(path)
+        self._base_path = Path(base_path)
         self._chunk_size = chunk_size
         return super().__init__(
             status=status,
@@ -27,27 +27,31 @@ class ZipResponse(web.StreamResponse):
     async def prepare(self, request: web.BaseRequest) -> None:
         await super().prepare(request)
 
-        async for chunk in async_stream_zip(self.yield_members()):
+        async for chunk in async_stream_zip(self.yield_member_files()):
             await self.write(chunk)
 
         await self.write_eof()
 
-    async def yield_members(self) -> AsyncGenerator[AsyncMemberFile, None]:
-        for sub_path in self._path.glob("**/*"):
-            lstat = sub_path.lstat()
+    async def yield_member_paths(self) -> AsyncGenerator[Path, None]:
+        for member_path in self._base_path.glob("**/*"):
+            yield member_path
+
+    async def yield_member_files(self) -> AsyncGenerator[AsyncMemberFile, None]:
+        async for member_path in self.yield_member_paths():
+            lstat = member_path.lstat()
             yield (
-                str(sub_path.relative_to(self._path)),
+                str(member_path.relative_to(self._base_path)),
                 datetime.fromtimestamp(lstat.st_mtime),
                 lstat.st_mode,
                 ZIP_32,
-                self.yield_contents(sub_path),
+                self.yield_member_chunks(member_path),
             )
 
-    async def yield_contents(self, file_path: Path) -> AsyncGenerator[bytes, None]:
-        if file_path.is_dir():
+    async def yield_member_chunks(self, path: Path) -> AsyncGenerator[bytes, None]:
+        if path.is_dir():
             return
 
-        with file_path.open("rb") as file:
+        with path.open("rb") as file:
             while True:
                 chunk = file.read(self._chunk_size)
                 if not chunk:
